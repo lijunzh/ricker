8bf86bf55179eaaaa767a2dc7097c00e3e32c6df
Lijun Zhu <gatechzhu@gmail.com>
2017-02-14 09:53:56 -0500

    Use function annotation and single output.

--- ricker/ricker.py
+++ ricker/ricker.py
@@ -1,37 +1,28 @@
+import warnings
+
 import numpy as np
 
 
-def ricker(f=10, len=0.5, dt=0.002, peak_loc=0.25):
+def ricker(f: float = 10, len: float = 0.5, dt: float = 0.002,
+           peak_loc: float = 0.25) -> np.ndarray:
+    """ricker creates a shifted causal ricker wavelet (Maxican hat).
+
+    :param f: center frequency of Ricker wavelet (default 10)
+    :param len: float
+    :type len: signal length in unit of second (default 0.5 sec)
+    :param dt: float
+    :type dt: time sampling interval in unit of second (default 0.002 sec)
+    :param peak_loc: float
+    :type peak_loc: location of wavelet peak in unit of second (default 0.25
+    sec)
+    :return: shifted Ricker wavelet starting from t=0.
+    :rtype: np.ndarray
+
+    Note that the returned signal always starts at t=0. For a different
+    starting point, it can be achieved by shifting the time vector instead.
+
+
     """
-    ------------------------------------------------------------
-    Generate ricker wavelet signal for seismic simulation
-    ------------------------------------------------------------
-
-    Input and output:
-        :param f: (float) peak frequency in Hz (default 10)
-        :param len: (float) length of signal in sec (default 0.5)
-        :param dt: (float) time resolution in sec (default 0.002)
-        :param peak_loc: (float) peak location in sec (default 0.25)
-
-        :return:
-        t (nparray): time sequence
-        y (nparray): ricker signal sequence
-
-    Examples:
-        This function can be called directly with no argument as following::
-
-            $ ricker_signal = seispy.ricker()
-
-        which returns a 10 Hz Ricker wavelet that peaked at 0.25 sec,  0.5
-        sec long and sampled at 500 Hz.
-
-        Users can generate different Ricker wavelet by tweaking the input
-        argument
-
-    References:
-        http://subsurfwiki.org/wiki/Ricker_wavelet
-    """
-
     # Import check
     if f <= 0:
         raise ValueError("Center frequency (f) needs to be positive.")
@@ -42,18 +33,24 @@ def ricker(f=10, len=0.5, dt=0.002, peak_loc=0.25):
     if dt <= 0:
         raise ValueError("Time interval (dt) needs to be positive.")
 
-    # Generate time sequence based on sample frequency/period, signal length
-    # and peak location
-    t = np.linspace(-peak_loc, len - peak_loc - dt, int(len / dt))
+    if len < peak_loc:
+        warnings.warn("The peak location is outside the signal range. All "
+                      "zero output will be provided.")
+        return np.zeros(int(len / dt))
+    else:
+        # Generate time sequence based on sample frequency/period, signal length
+        # and peak location
+        t = np.linspace(-peak_loc, len - peak_loc - dt, int(len / dt))
 
-    # Shift signal to the correct location
-    t_out = t + peak_loc  # time shift Ricker wavelet based on peak_loc
+        # Shift signal to the correct location
+        t_out = t + peak_loc  # time shift Ricker wavelet based on peak_loc
 
-    # Generate Ricker wavelet signal based on reference
-    y = (1 - 2 * np.pi ** 2 * f ** 2 * t ** 2) * np.exp(
-        -np.pi ** 2 * f ** 2 * t ** 2)
+        # Generate Ricker wavelet signal based on reference
+        y = (1 - 2 * np.pi ** 2 * f ** 2 * t ** 2) * np.exp(
+            -np.pi ** 2 * f ** 2 * t ** 2)
 
-    return t_out, y
+        return y
+
 
 if __name__ == '__main__':
     pass
